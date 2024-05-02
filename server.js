23659934e45c8a77b0cc5bde4013c4ad3826b560
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-05-02 21:25:09 +0800

    set up HTTP server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const dotenv = require("dotenv");
+const http = require("http");
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 dotenv.config({ path: "./config.env" });
+
+// DB
 
 const connectionString = process.env.DATABASE.replace(
     '<password>',
@@ -12,3 +16,57 @@ mongoose.connect(connectionString).then(() => {
     console.log("successfully connected to DB");
 });
 
+// HTTP server
+
+const httpListenPort = process.env.PORT || 3005;
+
+const responseHeaders = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+};
+
+/**
+ * Sends "successful" response.
+ * @param {*} res 
+ * @param {*} data If is not `null`, this is the value of key "data" in the response body. 
+ *                 If is `null`, the response has no body.
+ */
+function respondAsSuccessful(res, data) {
+    res.writeHead(200, responseHeaders);
+    if (data !== null) {
+        res.write(JSON.stringify({
+            status: "sucessful",
+            data: data
+        }));
+    }
+    res.end();
+}
+
+/**
+ * Sends "failed" response.
+ * @param {*} res 
+ * @param {Number} statusCode 
+ * @param {String} message 
+ */
+function respondAsFailed(res, statusCode, message) {
+    res.writeHead(statusCode, responseHeaders);
+    res.write(JSON.stringify({
+        status: "failed",
+        message: message
+    }));
+    res.end();
+}
+
+function httpListener(req, res) {
+    let body = "";
+    req.on("data", (chunck) => { 
+        body += chunck; 
+    });
+
+    //
+
+}
+
+http.createServer(httpListener).listen(httpListenPort);
